a65492d8933613d4b8b1f8b5456681db75e7425c
Nevidimka655 <alex.kobrys@gmail.com>
2024-08-14 23:23:54 +0300

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,31 @@
+@Suppress("DSL_SCOPE_VIOLATION") // TODO: Remove once KTIJ-19369 is fixed
+plugins {
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "com.nevidimka655.tiles_with_coroutines"
+    compileSdk = project.property("compileSdk").toString().toInt()
+
+    defaultConfig {
+        minSdk = project.property("minSdk").toString().toInt()
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = true
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+}
+
+dependencies {
+    api(libs.kotlin.coroutines)
+}
---
+++ src/main/kotlin/com/nevidimka655/tiles_with_coroutines/TileServiceCoroutine.kt
@@ -0,0 +1,27 @@
+package com.nevidimka655.tiles_with_coroutines
+
+import android.annotation.SuppressLint
+import android.service.quicksettings.TileService
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.SupervisorJob
+import kotlin.coroutines.CoroutineContext
+
+@SuppressLint("NewApi")
+open class TileServiceCoroutine : TileService(), CoroutineScope {
+    private lateinit var job: Job
+
+    override val coroutineContext: CoroutineContext get() = Dispatchers.Main + SupervisorJob(job)
+
+    override fun onCreate() {
+        super.onCreate()
+        job = Job()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        job.cancel()
+    }
+
+}
