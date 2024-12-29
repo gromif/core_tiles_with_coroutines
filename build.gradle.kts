afc8128ff2a74770cb799d0a0e47cc81e3db9fae
Nevidimka655 <alex.kobrys@gmail.com>
2024-12-29 14:21:35 +0200

    Replace coroutines via convention plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,8 @@
 plugins {
     alias(libs.plugins.astracrypt.android.library)
+    alias(libs.plugins.astracrypt.kotlin.coroutines)
 }
 
 android {
     namespace = "com.nevidimka655.tiles_with_coroutines"
-}
-
-dependencies {
-    api(libs.kotlin.coroutines)
 }
